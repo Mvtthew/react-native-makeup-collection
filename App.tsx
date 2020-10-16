65ffda13eabd9f97696e2bc4a5cf1cdd17f17829
Mvtthew <mateusz2345678@gmail.com>
2020-10-16 11:56:51 +0200

    :wrench: React router + home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
+import {NativeRouter, Route} from 'react-router-native';
+import Home from './screens/Home';
 
 const App = () => {
   return (
     <>
-      <StatusBar backgroundColor="teal" />
+      <StatusBar backgroundColor="#80afdd" />
       <SafeAreaView>
-        <Text>Hello world</Text>
+        <NativeRouter>
+          <Route path="/" exact component={Home} />
+        </NativeRouter>
       </SafeAreaView>
     </>
   );
